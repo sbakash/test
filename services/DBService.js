1461e61711aad763a900bcc97d619015f77340bf
sbakash <sbakash@gmail.com>
2018-01-21 16:11:27 +0530

    2;

--- services/DBService.js
+++ services/DBService.js
@@ -1,7 +1,7 @@
 var MongoClient=require('mongodb').MongoClient;
 
 exports.createConnection=function(){
-  MongoClient.connect("mongodb://sbakash:sbakash@ds047325.mlab.com:47325/projector13").then(function(client){
+  MongoClient.connect("mongodb://sbakas:sbakas@ds047325.mlab.com:47325/projector13").then(function(client){
     console.log("Connected to mongodb");
     exports.database=client.db("projector13");
     //console.log(database);
